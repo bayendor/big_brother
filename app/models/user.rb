3fe82098830d7d7b4df0f751b6116eb67a3c1bc9
Viki Harrod <victoria.harrod@gmail.com>
2015-03-12 10:25:41 -0600

    move find-or-creating and saving of user to the model level

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
+  def self.from_omniauth(hash)
+    user         = first_or_create(github_id: hash["uid"])
+    user.update_from_auth(hash)
+    user
+  end
+
   def update_from_auth(hash)
     self.name    = hash["info"]["name"]
     self.picture = hash["info"]["image"]
+    save!
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,9 +10,7 @@ RSpec.describe User, type: :model do
                      "credentials"=>{"token"=>"b6ccdc2e4d491f97468e1724a5e3037bd285f254", "expires"=>false}}
            }
 
-    user = User.create!(github_id: hash["uid"])
-    user.update_from_auth(hash)
-    user.save!
+    user = User.from_omniauth(hash)
 
     expect(user.reload.name).to eq("Somebody Else")
   end
