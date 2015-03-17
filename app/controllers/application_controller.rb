f0c4352ff8fae1f7c460daf3ccfd968267efea90
David Bayendor <dbayendor@gmail.com>
2015-03-16 20:23:14 -0600

    Setup user controllers and view templates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user
+  helper_method :user_signed_in?
+  helper_method :correct_user?
   after_action :clean_session
 
   def clean_session
@@ -12,6 +12,25 @@ class ApplicationController < ActionController::Base
   def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+
+  private
+
+  def user_signed_in?
+    return true if current_user
+  end
+
+  def correct_user?
+    @user = User.find(params[:id])
+    unless current_user == @user
+      redirect_to root_url, alert: 'Access denied.'
+    end
+  end
+
+  def authenticate_user!
+    unless current_user
+      redirect_to root_url, alert: 'You need to sign in to access this page.'
     end
   end
 end
