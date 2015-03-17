182480dfc301530ab5bf72715c519a3cb042996d
David Bayendor <dbayendor@gmail.com>
2015-03-16 21:06:29 -0600

    Experimental layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   # before_filter :authenticate_user!
   before_filter :correct_user?, except: [:index]
+  before_action :find_user, only: [:index, :show]
 
   def index
     @users = User.all
@@ -8,5 +9,16 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :commands, :picture)
+  end
+
+  def find_user
+    # @user = User.find(params[:id])
+    @user = User.find(1)
   end
 end
