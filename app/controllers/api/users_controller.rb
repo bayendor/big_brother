534ecae848f79fb980e2311b4dd3766edebb4032
Viki Harrod <victoria.harrod@gmail.com>
2015-03-16 19:41:45 -0600

    add update action and spec for uploading user command histories from big_bro

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,7 +2,7 @@ class Api::UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def update
-    @user = User.find_by(github_id: our_params["api_key"])
+    @user = User.find_by(github_id: big_bro_params["api_key"])
     @user.update(commands: merge_in_new_commands(@user))
     if @user.save
       render json: @user
@@ -13,11 +13,11 @@ class Api::UsersController < ApplicationController
 
   def merge_in_new_commands(user)
     current_commands = user.commands
-    new_commands     = our_params["commands"]
+    new_commands     = big_bro_params["commands"]
     current_commands.deep_merge(new_commands) {|key, old, new| old + new}
   end
 
-  def our_params
+  def big_bro_params
     JSON.parse(params["big_bro"])
   end
 end
--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Api::UsersController, type: :controller do
-  # big_bro='{"api_key":"5874167", "commands":{"sudo":{"other":1}}}'
+  describe "PUT update" do
+    it "updates a user's commands" do
+      user = User.create!(name: "Test User", github_id: "abc123")
+      patch :update, "big_bro"=>'{"api_key":"abc123", "commands":{"sudo":{"other":1}}}'
 
+      expect(user.reload.commands).to eq({"sudo"=> {"other"=>1}})
+    end
+  end
 end
