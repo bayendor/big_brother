8cb1090d9d384098cfd1e4cf820dd007c692ba93
Alex <96dalex@gmail.com>
2015-03-18 17:56:12 -0600

    fixed the test

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require "tmpdir"
 
 RSpec.describe Api::UsersController, type: :controller do
   describe "PUT update" do
@@ -6,7 +7,11 @@ RSpec.describe Api::UsersController, type: :controller do
       user = User.create!(name: "Test User")
       user.api_key.access_token = "abc123"
       user.api_key.save!
-      put :update, "big_bro"=>'{"api_key":"abc123", "commands":{"sudo":{"other":1}}}'
+
+      temp_path = File.expand_path "temp.json", Dir.tmpdir
+      File.write temp_path, '{"api_key":"abc123", "commands":{"sudo":{"other":1}}}'
+
+      put :update, file: Rack::Test::UploadedFile.new(temp_path, "text/json")
 
       expect(user.reload.commands).to eq({"sudo"=> {"other"=>1}})
     end
