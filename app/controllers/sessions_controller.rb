a45940350f70f4845ea525f439eebb2eeef7062d
Viki Harrod <victoria.harrod@gmail.com>
2015-03-18 10:28:51 -0600

    link 'your profile' on getting_started to login if unauth'd, change redirect on login to user_path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     if login_successful?
       @user = User.from_omniauth(request.env["omniauth.auth"])
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to user_path(@user)
     end
   end
 
