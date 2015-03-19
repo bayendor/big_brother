3394197e6fcdd2233e42a9225c54043195a22a9f
Viki Harrod <victoria.harrod@gmail.com>
2015-03-19 11:33:09 -0600

    add login_count to user so we can redirect to different places based on new_user status

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150317163740) do
+ActiveRecord::Schema.define(version: 20150319171547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,9 +28,10 @@ ActiveRecord::Schema.define(version: 20150317163740) do
     t.string   "github_id"
     t.string   "name"
     t.string   "picture"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.hstore   "commands",   default: {}
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.hstore   "commands",    default: {}
+    t.integer  "login_count", default: 0
   end
 
 end
